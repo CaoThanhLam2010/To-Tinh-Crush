6ab0942b508bf1f5d2de026a770e1d3b39f5c7e8
CaoThanhLam2010 <89892369+CaoThanhLam2010@users.noreply.github.com>
2021-09-02 12:09:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
+    introTitle: 'Nguyễn Thị Khánh Huyền',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    desc: 'Cậu ko đc thoát đâu đấy nếu cậu thoát là cậu đồng ý làm zợ mik rồi nha ',
+    btnYes: 'mik iu cậu nhìu lắm <33',
+    btnNo: Cậu mơ à😒',
+    question:'Cậu nói lý do cậu thích mik ik <3',
+    btnReply: 'Gửi cho cậu <3',
+    reply: 'Vì cậu đẹp zai lại còn cute nx <33333333',
+    mess: 'Mik biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
